2cf9a0f16971e75c0178c4e28ddc064b4d0179f1
liukai <869896679@qq.com>
2021-09-18 11:41:36 +0800

    客户端程序支持ca证书双向认证

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"grpcCli/services"
+	"io/ioutil"
 	"log"
 )
 
 func main()  {
-	creds,err := credentials.NewClientTLSFromFile("keys/ssl.crt","kai.com")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//creds,err := credentials.NewClientTLSFromFile("keys/ssl.crt","kai.com")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	//加载客户端证书
+	cert,_ := tls.LoadX509KeyPair("cert/client.pem","cert/client.key")
+	//加载ca证书，双向验证，客户端ca证书主要用来验证服务端证书是否合法
+	certPool := x509.NewCertPool()
+	ca,_ := ioutil.ReadFile("cert/ca.pem")
+	certPool.AppendCertsFromPEM(ca)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName: "localhost",
+		RootCAs: certPool,
+	})
+
 	//向指定端口发起连接
 	conn,err := grpc.Dial(":8081",grpc.WithTransportCredentials(creds))
 	if err != nil {
